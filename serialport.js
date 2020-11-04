a85844f5bb10057ca1c104de3d9a782aadd5c498
Jesper Nilsson <jesper@klandestino.se>
2020-11-04 17:29:11 +0100

    Better serial

--- serialport.js
+++ serialport.js
@@ -24,27 +24,48 @@ db.defaults({ recipes: [], logs: [] }).write();
  * Setup Serial Port.
  */
 if ( process.env.SERIAL_PORT !== "" ) {
-        var SerialPort = require('serialport');
-        var Readline = require('@serialport/parser-readline');
-        var port = new SerialPort(process.env.SERIAL_PORT, {
+    var SerialPort = require('serialport');
+    var Readline = require('@serialport/parser-readline');
+    var port = new SerialPort(process.env.SERIAL_PORT, {
         baudRate: 9600
-        });
-        var parser = port.pipe(new Readline({ delimiter: '\r\n' }));
+    });
+    var parser = port.pipe(new Readline({ delimiter: '\r\n' }));
 
-        /**
-         * Read the serialPort data from Arduino.
-         */
-        parser.on('data', (data) => {
-                // Get last log entry and the moment one hour after that.
-                db.read();
-                let lastLog = db.get('logs').takeRight(1).value().pop();
-                let nextMoment = typeof lastLog !== "undefined" ? moment(lastLog.datetime).add(1, 'hours') : moment().add(1, 'hours');
+    /**
+     * Read the serialPort data from Arduino.
+     */
+    parser.on('data', (data) => {
+        // Set default setTemp higher than room temperature.
+        let setTemp = 99;
 
-                console.log( { datetime: moment().format(), temp: data } );
-                // If one hour passed since last log entry.
-                if(moment().isAfter(nextMoment, 'hour') ) {
-                        // Write new log entry.
-                        db.get('logs').push({ datetime: moment().format(), temp: data }).write();
-                }
+        // Get last log entry and the moment one hour after that.
+        db.read();
+
+        // Get active recipe
+        let recipe = db.get('recipes').filter( item => {
+            return item.activated !== false
+        }).value().shift();
+
+        // Get and Loop through recipe steps in reverse.
+        let steps = recipe.steps.reverse();
+        steps.forEach( step => {
+            // If step are not after this moment + step time set setTemp.
+            let stepdate = moment(recipe.activated).add(step.stepTime, 'days');
+            if(moment().isBefore(stepdate) ) {
+                setTemp = step.stepTemp;
+            }
         });
+
+        // Send setTime to SerialPost.
+        port.write(setTemp+'\r\n')
+
+        let lastLog = db.get('logs').takeRight(1).value().pop();
+        let nextMoment = typeof lastLog !== "undefined" ? moment(lastLog.datetime).add(1, 'hours') : moment().add(1, 'hours');
+
+        // If one hour passed since last log entry.
+        if(moment().isAfter(nextMoment, 'hour') ) {
+                // Write new log entry.
+                db.get('logs').push({ datetime: moment().format(), temp: data }).write();
+        }
+    });
 }
