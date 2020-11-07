800ddf57e126aa8a55ad2f6d4d6827d41690ac25
Jesper Nilsson <jesper@klandestino.se>
2020-11-07 17:29:11 +0100

    Adds temp route.

---
+++ routes/temp.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+var low = require('lowdb');
+var FileSync = require('lowdb/adapters/FileSync');
+var fs = require('fs');
+
+/* GET logs listing. */
+router.get('/', function(req, res, next) {
+	var adapter = new FileSync('db.json');
+	var db = low(adapter);
+	var response = db.get('temp').value();
+	res.json(response);
+});
+
+module.exports = router;
